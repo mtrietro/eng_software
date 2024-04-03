e53c4fccd952f324f685b373463a6914458a5b3e
Guilherme Fernandes <guifernandes111@gmail.com>
2024-04-03 20:20:55 -0300

    Update cadastroDvd.ts

--- eng_software/cadastroDvd.ts
+++ eng_software/cadastroDvd.ts
@@ -4,7 +4,4 @@ import { DVD } from "./dvd";
 class CadastroDVD {
     private dvds: DVD[]
 
-    public inclui(amigo: Amigo) {}
-    public altera(id: number) {}
-    public exclui(id: number) {}
 }
